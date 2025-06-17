241cf84f062734af6b65204a1b18f11790c42f04
Kieran Convey <kc914@student.aru.ac.uk>
2025-06-17 13:08:22 +0000

    Data Building

--- car_project/tests/test_transform.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-from src.transform import clean_car_data
-
-def test_clean_car_data():
-    spark = SparkSession.builder.appName("Test").getOrCreate()
-    test_data = [("22–30", "Toyota", "Corolla"), ("19–25", "Honda", "Civic")]
-    df = spark.createDataFrame(test_data, ["MPG", "brand", "Model"])
-    cleaned = clean_car_data(spark, df)
-    assert "MPG_avg" in cleaned.columns
